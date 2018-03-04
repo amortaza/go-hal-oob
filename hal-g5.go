04bb7dff0f90cfd4e2b485f05c8aedbd4008b8af
Afshin Mortazavi-Nia <davidnia1@gmail.com>
2018-03-04 15:48:59 -0700

    .

--- hal-g5.go
+++ hal-g5.go
@@ -2,33 +2,34 @@ package hal_g5
 
 import (
 	"github.com/amortaza/go-xel"
-	"github.com/amortaza/go-bellina"
 	"github.com/amortaza/go-g5"
-	"fmt"
+	"github.com/amortaza/go-bellina"
 )
 
 type Hal struct {
 }
 
 func New() *Hal {
+
 	return &Hal{}
 }
 
-func (hal *Hal) Start(	width, height int,
-			title string,
-			onAfterGL,
-			onLoop,
-			onBeforeDelete func(),
-			onResize,
-			onMouseMove func(int,int),
-			onMouseButton func(bl.MouseButton, bl.ButtonAction),
-			onKey func(bl.KeyboardKey, bl.ButtonAction, bool, bool, bool)) {
+func (hal *Hal) Start(
+		width, height int,
+		title string,
+		onAfterGL,
+		onLoop,
+		onBeforeDeleteWindow func(),
+		onResize,
+		onMouseMove func(int,int),
+		onMouseButton func(xel.MouseButton, xel.ButtonAction),
+		onKey func(xel.KeyboardKey, xel.ButtonAction, bool, bool, bool)) {
 
-	xel.Init(title, width, height)
+	xel.Init(width, height)
 
 	xel.SetCallbacks(
+
 		func() {
-			fmt.Println("(+) g5.Init()")
 
 			g5.Init()
 
@@ -40,50 +41,55 @@ func (hal *Hal) Start(	width, height int,
 		onLoop,
 
 		func() {
-			if onBeforeDelete != nil {
-				onBeforeDelete()
-			}
 
-			fmt.Println("(-) g5.Uninit()")
+			if onBeforeDeleteWindow != nil {
+				onBeforeDeleteWindow()
+			}
 
 			g5.Uninit()
 		},
 
 		onResize, onMouseMove, onMouseButton, onKey)
 
-	xel.Loop()
-
-	xel.Uninit()
+	xel.Loop(title)
 }
 
 func (hal *Hal) NewCanvas(width, height int) bl.Canvas {
+
 	return g5.NewCanvas(width, height)
 }
 
-func (hal *Hal) Clear(r, g, b, a float32) {
-	g5.Clear(r, g, b, a)
+func (hal *Hal) Clear(r, g, b float32) {
+
+	g5.Clear(r, g, b)
 }
 
 func (hal *Hal) PushView(width, height int) {
+
 	g5.PushView(width, height)
 }
 
 func (hal *Hal) PopView() {
+
 	g5.PopView()
 }
 
 func (hal *Hal) GetGraphics() bl.Graphics {
+
 	return hal
 }
 
 func (hal *Hal) GetWindowDim()(width, height int) {
+
 	return xel.WinWidth, xel.WinHeight
 }
 
 func (hal *Hal) GetMousePos()(x,y int) {
+
 	return xel.MouseX, xel.MouseY
 }
 
 func (hal *Hal) SetMouseCursor(cursor bl.MouseCursor) {
+
 	xel.SetMouseCursor(cursor)
 }
