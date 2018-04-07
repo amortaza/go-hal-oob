7d13fe859b5fd94ad42d90f09c475d9f6eca85ee
Afshin Mortazavi-Nia <davidnia1@gmail.com>
2018-04-06 23:07:51 -0700

    .

--- example/example-hal-g5-simple.go
+++ example/example-hal-g5-simple.go
@@ -7,9 +7,29 @@ import (
 
 var hal *hal_g5.Hal
 
+var first = true
+
 func onLoop() {
 
-	hal.GetGraphics().Clear(1,0,0)
+	if first {
+		first = false
+	} else {
+		return
+	}
+	//hal.GetGraphics().Clear(1,0,0)
+
+	w, h := hal.GetWindowDim()
+	g_graphics := hal.GetGraphics()
+
+	g_graphics.PushView(w, h)
+
+	g_graphics.Clear(.3, .3, .3)
+
+	//canvas := renderCanvas(Root_Node)
+
+	//canvas.Paint(false, 0, 0, four_ones_float32)
+
+	g_graphics.PopView()
 }
 
 func init() {
